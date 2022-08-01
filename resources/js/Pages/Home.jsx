b8a0e0f7565cfb90d6c7e8b7a774b4c32653e99e
Daniel <20521181@gm.uit.edu.vn>
2022-08-01 16:35:01 +0700

    feat: project setup

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link, Head } from '@inertiajs/inertia-react';
+
+export default function Home() {
+    return (
+        <>
+            <Head title="Home" />
+            <h1>Hello World</h1>
+        </>
+    );
+}
